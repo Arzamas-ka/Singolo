f7426972cbb2b71ed052137932ec77e90a26e27c
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-03-16 16:23:12 +0300

    feat: implement slider

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const navList = document.querySelector('.navigation__list');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
 
 const activeNav = () => {
   navList.addEventListener('click', evt => {
@@ -10,5 +12,43 @@ const activeNav = () => {
 
   return;
 };
-
 activeNav();
+
+const slider = () => {
+  const slides = Array.from(document.querySelectorAll('.banner__slider'));
+  let index = 0;
+  let currentSlide = slides[index];
+
+  arrowLeft.addEventListener('click', () => {
+    if (index === 0) {
+      currentSlide.classList.remove('banner__slider--show');
+      currentSlide = slides[slides.length - 1];
+      currentSlide.classList.add('banner__slider--show');
+      index = slides.length - 1;
+
+      return;
+    }
+
+    index -= 1;
+    currentSlide.classList.remove('banner__slider--show');
+    currentSlide = slides[index];
+    currentSlide.classList.add('banner__slider--show');
+  });
+
+  arrowRight.addEventListener('click', () => {
+    if (index === 0) {
+      currentSlide.classList.remove('banner__slider--show');
+      currentSlide = slides[slides.length - 1];
+      currentSlide.classList.add('banner__slider--show');
+      index = slides.length - 1;
+
+      return;
+    }
+
+    index -= 1;
+    currentSlide.classList.remove('banner__slider--show');
+    currentSlide = slides[index];
+    currentSlide.classList.add('banner__slider--show');
+  });
+};
+slider();
